7d9662385bf4aa97beffb8fcc561dc43d99ad584
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-11-26 11:05:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-3vbyhd)
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [state, setState] = useState(0);
+  function handleClick() {
+    setState: (name, age) => {
+      console.log(rahul, 10);
+      (rahul, 10)
+    };
+  
+  }
+
+
+  function mathd (){
+    math.random()
+  }
+
+  return (
+    <div>
+      <p>click button {state}</p>
+      <button onClick={handleClick}>hjk</button>
+    </div>
+  );
+}
